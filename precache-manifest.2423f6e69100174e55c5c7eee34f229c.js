3ebdb8f7cfb565d82be40f3d2915763877d628d8
Mohit Sharma <mohit.u.sharma2407@gmail.com>
2020-08-10 19:42:40 +0530

    Updates

---
+++ precache-manifest.2423f6e69100174e55c5c7eee34f229c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee77b91f3779c611f781f310298bcf87",
+    "url": "/iamlatebloomer/index.html"
+  },
+  {
+    "revision": "5bae327743b843c567d1",
+    "url": "/iamlatebloomer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7dbb294aef415085f0a8",
+    "url": "/iamlatebloomer/static/js/2.886465ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iamlatebloomer/static/js/2.886465ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bae327743b843c567d1",
+    "url": "/iamlatebloomer/static/js/main.772bcae1.chunk.js"
+  },
+  {
+    "revision": "570fb394c7177ad0900d",
+    "url": "/iamlatebloomer/static/js/runtime-main.ce99eb06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iamlatebloomer/static/media/logo.5d5d9eef.svg"
+  }
+]);
