ef5ead753a42f0ed512fb395ab5938c2bb32db11
Mohit Sharma <mohit.u.sharma2407@gmail.com>
2020-08-10 22:30:52 +0530

    its for u

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import imgForU from './moon.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Sun flower, keep smiling</h1>
+      <h2> this is for u</h2>
+      <img src={imgForU} alt="Girl in a jacket" width="500" height="600"/>
     </div>
   );
 }
